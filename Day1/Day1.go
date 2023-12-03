db59ef0effa49ac3ee19583e1bfb4a06b348178e
Bjoergermeister <bjarnesvensson@web.de>
2023-12-03 14:11:09 +0100

    Day 1 Puzzle 2

--- Day1/Day1.go
+++ Day1/Day1.go
@@ -3,12 +3,13 @@ package main;
 import "os";
 import "bufio";
 import "fmt";
+import "strings";
 
 func check(error error){
 
 }
 
-func Puzzle1(lines [][]byte) int {
+func Puzzle1(lines []string) int {
 	total := 0;
 	for _, line := range lines {
 		var onlyNumbers []byte;
@@ -25,18 +26,69 @@ func Puzzle1(lines [][]byte) int {
 	return total;
 }
 
+func getNumberAtPosition(line string, position int) int {
+	digitsAsWords := []string{ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+	// first check if the line starts with an actual digit
+	if line[position] >= '0' && line[position] <= '9' {
+		return int(line[position] - '0');
+	}
+	
+	// first check if the line starts with one of the numbers
+	for index, digit := range digitsAsWords {
+		if strings.HasPrefix(line[position:], digit) {
+			return index + 1;
+		}	
+	}
+
+	return -1;
+}
+
+func Puzzle2(lines []string) int {
+	total := 0;
+	
+	for _, line := range lines {
+		
+		firstNumber := -1;
+		lastNumber := -1;
+
+		// search for first number from the start
+		for i := 0; i < len(line); i++ {
+			number := getNumberAtPosition(line, i);
+			if (number != -1) {
+				firstNumber = number;
+				break;
+			}
+		}
+
+		// search for last number from the back
+		for i := len(line) - 1; i >= 0; i-- {
+			number := getNumberAtPosition(line, i);
+			if (number != -1) {
+				lastNumber = number;
+				break;
+			}
+		}
+
+		total += firstNumber * 10 + lastNumber;
+	}
+
+	return total;
+}
+
 func main(){
 	file, _ := os.Open("input.txt");
 	defer file.Close();
 
 	scanner := bufio.NewScanner(file);
 
-	var lines [][]byte;
+	var lines []string;
 	for scanner.Scan() {
 		line := scanner.Text();
-		lineAsBytes := []byte(line);
+		lineAsBytes := line;
 		lines = append(lines, lineAsBytes);
 	}
 
-	fmt.Printf("Puzzle 1: %d", Puzzle1(lines));
+	fmt.Printf("Puzzle 1: %d\n", Puzzle1(lines));
+	fmt.Printf("Puzzle 2: %d\n", Puzzle2(lines));
 }
