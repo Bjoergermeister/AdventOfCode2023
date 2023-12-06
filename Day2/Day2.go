d06ea938df8f3bb191b8e2fcb16ed1a1d6f98559
Bjoergermeister <b.svensson@ostfalia.de>
2023-12-06 20:27:18 +0100

    Day 2 Puzzle 2

--- Day2/Day2.go
+++ Day2/Day2.go
@@ -1,76 +1,93 @@
-package main;
+package main
 
-import "os";
-import "fmt";
-import "bufio";
-import "strings";
-import "strconv";
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
-var maxRedCubes int = 12;
-var maxGreenCubes int = 13;
-var maxBlueCubes int = 14;
+var maxRedCubes int = 12
+var maxGreenCubes int = 13
+var maxBlueCubes int = 14
 
 type CubeSet struct {
-	green int;
-	red int;
-	blue int;
+	green int
+	red   int
+	blue  int
 }
 
 func parseSets(sets []string) []CubeSet {
-	var cubeSets []CubeSet;
+	var cubeSets []CubeSet
 	for _, set := range sets {
-		cubes := strings.Split(set, ",");
-		var cubeSet CubeSet;
+		cubes := strings.Split(set, ",")
+		var cubeSet CubeSet
 		for _, cube := range cubes {
-			cube = strings.TrimSpace(cube);
-			divider := strings.Index(cube, " ");
-			amount, _ := strconv.Atoi(cube[0:divider]);
-			color := cube[divider + 1:];
+			cube = strings.TrimSpace(cube)
+			divider := strings.Index(cube, " ")
+			amount, _ := strconv.Atoi(cube[0:divider])
+			color := cube[divider+1:]
 
 			switch color {
 			case "red":
-				cubeSet.red = amount;
+				cubeSet.red = amount
 			case "green":
-				cubeSet.green = amount;
+				cubeSet.green = amount
 			case "blue":
-				cubeSet.blue = amount;
+				cubeSet.blue = amount
 			}
 		}
-		cubeSets = append(cubeSets, cubeSet);
+		cubeSets = append(cubeSets, cubeSet)
 	}
-	return cubeSets;
+	return cubeSets
 }
 
-func Puzzle1(lines []string) int {
-	total := 0;
-	for gameIndex, line := range lines {
-		parts := strings.Split(line, ":");
-
-		sets := parseSets(strings.Split(parts[1], ";"));
-
-		gameIsValid := true;
-		for _, set := range sets {
+func Puzzle1(games [][]CubeSet) int {
+	total := 0
+	for gameIndex, game := range games {
+		gameIsValid := true
+		for _, set := range game {
 			if set.red > maxRedCubes || set.green > maxGreenCubes || set.blue > maxBlueCubes {
-				gameIsValid = false;
+				gameIsValid = false
 			}
 		}
 
-		if (gameIsValid){
-			total += (gameIndex + 1);
+		if gameIsValid {
+			total += (gameIndex + 1)
 		}
 	}
-	return total;
+	return total
+}
+func Puzzle2(games [][]CubeSet) int {
+	total := 0
+	for _, game := range games {
+		lowestRedCubes := 0
+		lowestGreenCubes := 0
+		lowestBlueCubes := 0
+
+		for _, set := range game {
+			lowestBlueCubes = max(lowestBlueCubes, set.blue)
+			lowestRedCubes = max(lowestRedCubes, set.red)
+			lowestGreenCubes = max(lowestGreenCubes, set.green)
+		}
+
+		total += lowestBlueCubes * lowestGreenCubes * lowestRedCubes
+	}
+	return total
 }
 
-func main(){
-	file, _ := os.Open("input.txt");
+func main() {
+	file, _ := os.Open("input.txt")
 
-	var lines []string;
-	scanner := bufio.NewScanner(file);
-	for scanner.Scan(){
-		line := scanner.Text();
-		lines = append(lines, line);
+	var games [][]CubeSet
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		sets := strings.Split(line, ":")[1]
+		games = append(games, parseSets(strings.Split(sets, ";")))
 	}
 
-	fmt.Printf("Puzzle 1: %d\n", Puzzle1(lines));
+	fmt.Printf("Puzzle 1: %d\n", Puzzle1(games))
+	fmt.Printf("Puzzle 1: %d\n", Puzzle2(games))
 }
