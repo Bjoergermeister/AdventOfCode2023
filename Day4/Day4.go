58fb7fa8cde0030764f108090b084c6198a323c1
Bjoergermeister <bjarnesvensson@web.de>
2023-12-09 11:14:28 +0100

    Day 4 Puzzle 2

--- Day4/Day4.go
+++ Day4/Day4.go
@@ -7,6 +7,7 @@ import (
 	"slices"
 	"strconv"
 	"strings"
+	"math"
 )
 
 func parseStringToIntegerList(line string) []int {
@@ -22,22 +23,52 @@ func parseStringToIntegerList(line string) []int {
 	return result
 }
 
-func Puzzle1(lines []string) int {
-	total := 0
-	for _, line := range lines {
-		parts := strings.Split(line, "|")
-		winningNumbers := parseStringToIntegerList(parts[0])
-		myNumbers := parseStringToIntegerList(parts[1])
-
-		cardResult := 0.5
-		for _, winningNumber := range winningNumbers {
-			if slices.Contains(myNumbers, winningNumber) {
-				cardResult *= 2
-			}
+func countWinningNumbers(winningNumbers []int, myNumbers[] int) int {
+	count := 0
+	for _, winningNumber := range winningNumbers {
+		if slices.Contains(myNumbers, winningNumber) {
+			count++
 		}
+	}
 
-		if cardResult >= 1 {
-			total += int(cardResult)
+	return count
+}
+
+func getWinningNumberCountForLine(line string) int {
+	parts := strings.Split(line, "|")
+	winningNumbers := parseStringToIntegerList(parts[0])
+	myNumbers := parseStringToIntegerList(parts[1])
+	return countWinningNumbers(winningNumbers, myNumbers)
+}
+
+func Puzzle1(lines []string) int {
+	total := 0.0
+	for _, line := range lines {
+		winningNumberCount := getWinningNumberCountForLine(line)
+
+		if winningNumberCount > 0 {
+			total += math.Pow(2.0, float64(winningNumberCount - 1))
+		}
+	}
+
+	return int(total)
+}
+
+func Puzzle2(lines []string) int {
+	total := len(lines)
+	cardCounts := make([]int, len(lines))
+	for i := 0; i < len(lines); i++ {
+		cardCounts[i] = 1
+	}
+
+	for lineIndex, line := range lines {
+		winningNumberCount := getWinningNumberCountForLine(line)
+		
+		if winningNumberCount > 0 {
+			for i := lineIndex + 1; i < min(len(lines), lineIndex + 1 + winningNumberCount); i++ {
+				cardCounts[i] += cardCounts[lineIndex]
+				total += cardCounts[lineIndex]
+			}	
 		}
 	}
 
@@ -51,8 +82,9 @@ func main() {
 	var lines []string
 	for scanner.Scan() {
 		line := scanner.Text()
-		lines = append(lines, line[10:])
+		lines = append(lines, line[strings.Index(line, ":") + 1:])
 	}
 
 	fmt.Printf("Puzzle 1: %d\n", Puzzle1(lines))
+	fmt.Printf("Puzzle 2: %d\n", Puzzle2(lines))
 }
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Language: Go
 Day 1: <span style="color:yellow">&#11088;&#11088;</span>\
 Day 2: <span style="color:yellow">&#11088;&#11088;</span>\
 Day 3: <span style="color:yellow">&#11088;&#11088;</span>\
-Day 4: <span style="color:yellow">&#11088;</span>\
+Day 4: <span style="color:yellow">&#11088;&#11088;</span>\
 Day 5: <span style="color:yellow"></span>\
 Day 6: <span style="color:yellow"></span>\
 Day 7: <span style="color:yellow"></span>\
@@ -31,7 +31,7 @@ Day 23: <span style="color:yellow"></span>\
 Day 24: <span style="color:yellow"></span>\
 Day 25: <span style="color:yellow"></span>\
 \
-Total: 7 out of 50
+Total: 8 out of 50
 
 # Other years
 
